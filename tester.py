096e356d88fa32e52a5c2bcf4b21ae8cba4515e5
Yunxiang He <heyx1@shanghaitech.edu.cn>
2023-07-29 21:17:39 +0800

    Feat: Add a 800x800 Lego Output, and Modified tester

--- tester.py
+++ tester.py
@@ -9,19 +9,22 @@ def mse_to_psnr(mse):
     return 20 * np.log10(1 / np.sqrt(mse))
 
 def resize_image(img, res):
+    if res == 800:
+        return img
     return cv.resize(img, (res, res))
 
 scenes = ["chair", "drums", "ficus", "hotdog", "lego", "materials", "mic", "ship"]
-res = 400
+res = 800
 
 def PSNR(name:str, res = 400):
     path1 = f"./outputs/Test_{name}.png"
     path2 = f"./data/nerf_synthetic/{name}/test/r_0.png"
     img1 = np.array(cv.imread(path1) / 255., dtype=np.float32)
     img2 = np.array(cv.imread(path2) / 255., dtype=np.float32)
+    delta = np.abs(img1 - img2)
     img2 = resize_image(img2, res)
     return mse_to_psnr(compute_mse(img1, img2))
 
 if __name__ == "__main__":
-    scene = scenes[6]
-    print(PSNR(scene, res))
+    for scene in scenes:
+        print(PSNR(scene, res))
