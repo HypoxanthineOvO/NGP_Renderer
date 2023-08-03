c4df89c5108706e3b2b58f54ab4e165d4cfb132b
Yunxiang He <heyx1@shanghaitech.edu.cn>
2023-08-03 12:59:31 +0800

    Fix: Delete no used scripts file run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-scenes = ["chair", "drums","ficus","hotdog","lego","materials","mic","ship"]
-
-for s in scenes:
-    os.system(f"python main.py --scene {s} --w 400 --h 400 --steps 256")
